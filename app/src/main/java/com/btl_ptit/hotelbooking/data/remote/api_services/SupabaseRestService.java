945f70448fad65f708511a611834a919db07cf23
Khuat Huy Nhan <NhanKH.B22CN575@stu.ptit.edu.vn>
2026-04-01 22:24:19 +0700

    them register

--- app/src/main/java/com/btl_ptit/hotelbooking/data/remote/api_services/SupabaseRestService.java
+++ app/src/main/java/com/btl_ptit/hotelbooking/data/remote/api_services/SupabaseRestService.java
@@ -5,14 +5,14 @@ import com.btl_ptit.hotelbooking.data.model.User;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface SupabaseRestService {
 
-    // Example query:
-    // GET /rest/v1/users?email=eq.test@gmail.com&select=email,phone,full_name,avatar_url
     @GET("rest/v1/users")
     Call<List<User>> getUserByEmail(
         @Header("Authorization") String bearerToken,
@@ -20,12 +20,16 @@ public interface SupabaseRestService {
         @Query("select") String select
     );
 
-    // Preferred query (secure):
-    // GET /rest/v1/users?id=eq.<auth-uuid>&select=email,phone,full_name,avatar_url
     @GET("rest/v1/users")
     Call<List<User>> getUserById(
         @Header("Authorization") String bearerToken,
         @Query("id") String idEq,
         @Query("select") String select
+    );
+
+    @POST("rest/v1/users")
+    Call<Void> insertUser(
+        @Header("Authorization") String bearerToken,
+        @Body User user
     );
 }
