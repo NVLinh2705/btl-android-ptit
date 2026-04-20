27449c93ac7c67cf74a01c5be78c70fc5f8587b4
shormez <nguyenduclam478@gmail.com>
2026-04-20 17:18:56 +0700

    fix auth

--- app/src/main/java/com/btl_ptit/hotelbooking/data/remote/api_services/BookingRestService.java
+++ app/src/main/java/com/btl_ptit/hotelbooking/data/remote/api_services/BookingRestService.java
@@ -6,18 +6,21 @@ import java.util.List;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface BookingRestService {
 
+    @Headers("Auth: True")
     @GET("rest/v1/bookings")
     Single<List<MyBooking>> getListBooking(
             @Query("select") String select,
             @Query("limit") int limit,
             @Query("offset") int offset
     );
-
+    @Headers("Auth: True")
     @GET("rest/v1/bookings")
     Single<List<MyBooking>> getBookingDetail(
             @Query("id") String id,
