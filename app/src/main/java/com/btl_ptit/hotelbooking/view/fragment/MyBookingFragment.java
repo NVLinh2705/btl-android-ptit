beabebc4285205cc9cc6e00f4ed5b9ab396cb116
shormez <nguyenduclam478@gmail.com>
2026-04-24 20:56:27 +0700

    thay đổi vị trí quét mã

--- app/src/main/java/com/btl_ptit/hotelbooking/view/fragment/MyBookingFragment.java
+++ app/src/main/java/com/btl_ptit/hotelbooking/view/fragment/MyBookingFragment.java
@@ -17,24 +17,31 @@ import android.widget.Toast;
 import com.btl_ptit.hotelbooking.R;
 import com.btl_ptit.hotelbooking.data.model.MyBooking;
 import com.btl_ptit.hotelbooking.data.model.MyHotel;
+import com.btl_ptit.hotelbooking.data.model.Role;
+import com.btl_ptit.hotelbooking.data.model.UserRole;
 import com.btl_ptit.hotelbooking.data.remote.SupabaseClient;
 import com.btl_ptit.hotelbooking.data.remote.api_services.BookingRestService;
 import com.btl_ptit.hotelbooking.data.remote.api_services.HotelRestService;
+import com.btl_ptit.hotelbooking.data.remote.api_services.SupabaseRestService;
 import com.btl_ptit.hotelbooking.data.repository.MyBookingRepository;
+import com.btl_ptit.hotelbooking.data.session.SessionManager;
 import com.btl_ptit.hotelbooking.databinding.FragmentMyBookingBinding;
 import com.btl_ptit.hotelbooking.listener.OnBookingClickListener;
 import com.btl_ptit.hotelbooking.utils.paging.MyComparator;
 import com.btl_ptit.hotelbooking.view.activity.BookingHistoryDetailActivity;
 import com.btl_ptit.hotelbooking.view.activity.LoginActivity;
+import com.btl_ptit.hotelbooking.view.activity.QrScannerActivity;
 import com.btl_ptit.hotelbooking.view.adapter.BookingAdapter;
 import com.btl_ptit.hotelbooking.view.adapter.LoadStateAdapter;
 import com.btl_ptit.hotelbooking.view_model.paging.BookingViewModel;
 import com.btl_ptit.hotelbooking.view_model.paging.BookingViewModelFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -46,6 +53,8 @@ public class MyBookingFragment extends Fragment {
     private String TAG = "MyBookingFragmentTAG";
 
     private BookingRestService bookingRestService;
+
+    private SupabaseRestService supabaseRestService;
 
     private BookingViewModel bookingViewModel;
 
@@ -62,6 +71,7 @@ public class MyBookingFragment extends Fragment {
         mContext = getContext();
         bookingRestService = SupabaseClient.createService(BookingRestService.class);
         myBookingRepository = new MyBookingRepository(bookingRestService);
+        supabaseRestService = SupabaseClient.createService(SupabaseRestService.class);
         bookingViewModel = new ViewModelProvider(requireActivity(), new BookingViewModelFactory(myBookingRepository)).get(BookingViewModel.class);
         initBookingAdapter();
         initBooking();
@@ -79,6 +89,39 @@ public class MyBookingFragment extends Fragment {
             }
             bookingViewModel.filterByStatus(status);
         });
+        String userId = SessionManager.getInstance().getUser().getId();
+
+        compositeDisposable.add(
+                supabaseRestService.getRolesByUser("eq." + userId,
+                        "roles(name)")
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(response -> {
+                            List<String> roles = new ArrayList<>();
+                            if (response != null) {
+                                for (UserRole item : response) {
+                                    roles.add(item.getRoles().getName());
+                                    System.out.println(item.getRoles().getName());
+                                }
+                                if (roles.contains("HOST")) {
+                                    mFragmentMyBookingBinding.qrButton.setVisibility(View.VISIBLE);
+                                    mFragmentMyBookingBinding.qrButton.setOnTouchListener(
+                                            (v, event) -> {
+                                                Intent intent = new Intent(mContext, QrScannerActivity.class);
+                                                startActivity(intent);
+                                                return true;
+                                            }
+                                    );
+                                }else{
+                                    mFragmentMyBookingBinding.qrButton.setVisibility(View.GONE);
+                                }
+                            }
+                        }, throwable -> {
+                            Log.e(TAG, "Error occurred: " + throwable.getMessage());
+                        }
+                )
+
+        );
 
 
 
